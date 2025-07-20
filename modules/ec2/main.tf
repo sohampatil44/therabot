6f7a073b3d53fca6cfb560a658f6140128b8b3f6
Soham Patil <sohampatil@Sohams-MacBook-Air.local>
2025-07-20 21:17:18 +0530

    userdata

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,32 +1,31 @@
 resource "aws_instance" "ec21" {
-    ami = var.ami_id
-    instance_type =var.instance_type
-    subnet_id = var.subnet_id
-    vpc_security_group_ids = [var.security_group_id]
-    key_name= var.key_name
-    associate_public_ip_address  = true
-    credit_specification {
-      cpu_credits = "unlimited"
-    }
-    root_block_device {
-      volume_size = 20
-      volume_type = "gp2"
-    }
+  ami                         = var.ami_id
+  instance_type              = var.instance_type
+  subnet_id                  = var.subnet_id
+  vpc_security_group_ids     = [var.security_group_id]
+  key_name                   = var.key_name
+  associate_public_ip_address = true
 
-    
-    user_data  = file("${path.module}/../user_data.sh")
-    tags= {
-        Name = "ripensense-ec2"
-    }
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
 
-    connection {
-        type = "ssh"
-        user = "ec2-user"
-        private_key = file("/Users/sohampatil/ripensense/RipenSense/network-keypair.pem")
-        host = self.public_ip
-        timeout = "2m"
-    }
-   
-    
-  
+  root_block_device {
+    volume_size = 32          # ✅ Increased to 32 GB
+    volume_type = "gp3"       # ✅ Faster and more cost-effective
+  }
+
+  user_data = file("${path.module}/../user_data.sh")
+
+  tags = {
+    Name = "ripensense-ec2"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("/Users/sohampatil/ripensense/RipenSense/network-keypair.pem")
+    host        = self.public_ip
+    timeout     = "2m"
+  }
 }
