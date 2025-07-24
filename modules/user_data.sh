93f06d4b749c059eb8d4e9762743f709b1195fa1
Soham Patil <sohampatil@Sohams-MacBook-Air.local>
2025-07-24 11:04:09 +0530

    user scriot

--- modules/user_data.sh
+++ modules/user_data.sh
@@ -5,42 +5,37 @@ exec > /var/log/user-data.log 2>&1
 echo "Starting the user data script"
 
 sudo yum update -y
-
 sudo yum install -y docker git
 
 DOCKER_COMPOSE_VERSION=2.24.6
-mkdir -p ~/.docker/cli-plugins
+mkdir -p /home/ec2-user/.docker/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64 \
-  -o ~/.docker/cli-plugins/docker-compose
+  -o /home/ec2-user/.docker/cli-plugins/docker-compose
 
 chmod +x /home/ec2-user/.docker/cli-plugins/docker-compose
-
 chown -R ec2-user:ec2-user /home/ec2-user/.docker
-
-
 
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 
+# Ensure ec2-user owns the home folder
+chown -R ec2-user:ec2-user /home/ec2-user
 
+# Clone repo as ec2-user
+sudo -u ec2-user git clone https://github.com/sohampatil44/therabot.git /home/ec2-user/therabot
 
-cd /home/ec2-user
-
-git clone https://github.com/sohampatil44/therabot.git
+# Retry docker compose up as ec2-user
 cd /home/ec2-user/therabot
 
 RETRIES=5
 COUNT=0
-until sudo -u ec2-user /home/ec2-user/.docker/cli-plugins/docker compose up -d; do
-  echo "Dcoker compose failed, retrying in 15 seconds.."
+until sudo -u ec2-user /home/ec2-user/.docker/cli-plugins/docker-compose up -d; do
+  echo "Docker compose failed, retrying in 15 seconds.."
   sleep 15
   COUNT=$((COUNT + 1))
-  if [ "$COUNT" -ge "RETRIES" ]; then
+  if [ "$COUNT" -ge "$RETRIES" ]; then
     echo "Docker compose failed after $RETRIES attempts, exiting."
     exit 1
   fi  
 done
-
-
-
