f8ec1601aeb94b535f4e1dbe17a9da8be53dd15d
Soham Patil <sohampatil@Sohams-MacBook-Air.local>
2025-08-02 20:17:45 +0530

    user scriot

--- modules/user_data.sh
+++ modules/user_data.sh
@@ -3,6 +3,28 @@ exec > /var/log/user-data.log 2>&1
 
 echo "Starting the user data script"
 
+# ----------------------
+# MAXIMUM SWAP CREATION
+# ----------------------
+
+echo "Checking existing swap space..."
+if free | awk '/^Swap:/ {exit !$2}'; then
+    echo "Swap already exists. Skipping creation."
+else
+    echo "Creating 4G swap file..."
+    sudo fallocate -l 4G /swapfile || sudo dd if=/dev/zero of=/swapfile bs=1M count=4096
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
+    echo "vm.swappiness=20" | sudo tee -a /etc/sysctl.conf
+    echo "Swap successfully created and enabled."
+fi
+
+# ----------------------
+# Package Installations
+# ----------------------
+
 sudo yum update -y
 sudo yum install -y docker git
 
@@ -32,7 +54,6 @@ cd /home/ec2-user/therabot
 
 echo "Installing CloudWatch Agent.."
 sudo yum install -y amazon-cloudwatch-agent
-
 
 echo "Starting Cloudwatch agent with config.."
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
@@ -70,7 +91,7 @@ mkdir -p /etc/grafana/provisioning/datasources
 mkdir -p /etc/grafana/provisioning/dashboards
 mkdir -p /etc/grafana/provisioning/dashboards-json
 
-echo "Copying Grafana provsioning files..."
+echo "Copying Grafana provisioning files..."
 cp /home/ec2-user/therabot/scripts/grafana/provisioning/dashboards/dashboards.yaml /etc/grafana/provisioning/dashboards/
 cp /home/ec2-user/therabot/scripts/grafana/provisioning/dashboards-json/ec2-dashboard.json /etc/grafana/provisioning/dashboards-json/
 cp /home/ec2-user/therabot/scripts/grafana/provisioning/cloudwatch-datasource.yaml /etc/grafana/provisioning/datasources/
