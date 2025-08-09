1212ab2c520a206a87b170125e89452f753490ca
Soham Patil <sohampatil@Sohams-MacBook-Air.local>
2025-08-09 20:58:42 +0530

    added asg alb

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -173,7 +173,7 @@ resource "aws_launch_template" "therabot_template" {
 }
 
 resource "aws_autoscaling_group" "therabot_asg" {
-  desired_capacity = 2
+  desired_capacity = 1
   max_size = 3
   min_size = 1
   vpc_zone_identifier = [var.subnet_id]
